8d93268a77a733fcca01182691552b01bf50e02b
D3U52S <40864189+deusssss@users.noreply.github.com>
2021-06-16 19:22:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'PyLo3',
   packages = ['PyLo3'],
-  version = '1.0',
+  version = '1.1',
   license='cc0-1.0',
   description = 'creatore di alberi filogenetici',
   author = "Lorenzo D'Eusebio",
   author_email = 'deusebiolorenzo@gmail.com',
   url = 'https://github.com/deusssss/PyLo3',
-  download_url = 'https://github.com/deusssss/PyLo3/archive/refs/tags/1.0.tar.gz',
+  download_url = 'https://github.com/deusssss/PyLo3/archive/refs/tags/1.1.tar.gz',
   keywords = ["biopython", "philogenetic analysis", "web application", "python server", "university project", "bioinformatics"],
   install_requires=[
           'matplotlib',
